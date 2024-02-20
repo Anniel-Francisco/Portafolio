e5f53c3c0c0762e930e3e2dff8102c21d91b722c
Anniel <annielfranciscoreyes@gmail.com>
2024-02-20 03:14:21 -0400

    test

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -39,12 +39,12 @@ export default {
         {
           name: "QUASAR FRAMEWORK",
           icon: [],
-          img: "../assets/quasar-framework-icon.png",
+          img: "/src/assets/quasar-framework-icon.png",
         },
         {
           name: "TAILWIND CSS",
           icon: [],
-          img: "../assets/tailwind-css-icon.png",
+          img: "/src/assets/tailwind-css-icon.png",
         },
       ],
     };
