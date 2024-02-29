8d30eb8d4dde06947df3a9214a784f53d7d67f2a
Anniel <annielfranciscoreyes@gmail.com>
2024-02-29 00:15:53 -0400

    webpack implementation

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// webpack.config.js
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
