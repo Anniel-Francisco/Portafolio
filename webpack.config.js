138e7e2d29218c5297022509a4db4001cf5c0ca1
Anniel <annielfranciscoreyes@gmail.com>
2024-02-29 20:18:23 -0400

    aa

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,13 @@ const config = {
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
+  resolve: {
+    extensions: [".js", ".vue"],
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
 };
 
 module.exports = () => {
