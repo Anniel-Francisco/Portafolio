0767536b566e9f469338f7969bb39fb1ced15198
Anniel <annielfranciscoreyes@gmail.com>
2024-02-29 21:28:19 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const config = {
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "style-loader"],
+        use: [stylesHandler, "css-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
