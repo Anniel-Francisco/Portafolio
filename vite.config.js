8684923d712b69d5ca8d2ff55002d05a0f800b01
Anniel <annielfranciscoreyes@gmail.com>
2024-03-11 19:03:25 -0400

    Revert "webpack"
    This reverts commit 5ec56d56d85cdeeb94ffdb19eff04387c862e3c4.

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,21 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import dotenv from "dotenv";
+dotenv.config();
+const isDev = process.env.NODE_ENV === "development";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    server: {
+      port: isDev ? 3000 : 8080,
+    },
+    build: {
+      minify: !isDev,
     },
   },
 });
