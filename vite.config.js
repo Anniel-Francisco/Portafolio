164cdf08848a5e6805b84391cbfdb18f80d946ed
Anniel <annielfranciscoreyes@gmail.com>
2024-03-28 19:45:47 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import dotenv from "dotenv";
 
-dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    "process.env": process.env,
-  },
+
   build: {
     assetsInlineLimit: 0,
   },
